b5d0951d924853f4f2113ba2bc18a2d07543d4f6
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-08-09 23:12:59 +0900

    Initial Commit

---
+++ VirtualVacations/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VirtualVacations
+//
+//  Created by Norimasa Nabeta on 2012/08/09.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VirtualVacations/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VirtualVacations
+//
+//  Created by Norimasa Nabeta on 2012/08/09.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
